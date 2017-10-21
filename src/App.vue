9f7c976503d5aaf038a5c7746eef16cd07ba4774
Milan Peters <m.peters@graphicon.de>
2017-10-22 00:10:45 +0200

    add sass

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #555667;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  @import 'main.scss';
 </style>
